a564a23350a58bd43e81926622ff7c838dbabc52
Shanmuga <38732797+shanmuga3@users.noreply.github.com>
2022-05-16 12:16:57 +0530

    ability issue

--- src/Middleware/LaravelEntrustAbility.php
+++ src/Middleware/LaravelEntrustAbility.php
@@ -23,10 +23,10 @@ class LaravelEntrustAbility extends LaravelEntrustMiddleware
      * @param \Closure $next
      * @param $roles
      * @param $permissions
-     * @param bool $validateAll
+     * @param Array|null $options
      * @return mixed
      */
-    public function handle($request, \Closure $next, $roles, $permissions, $validateAll = false)
+    public function handle($request, \Closure $next, $roles, $permissions, $options = null)
     {
         if (!is_array($roles)) {
             $roles = explode(self::DELIMITER, $roles);
@@ -36,11 +36,14 @@ class LaravelEntrustAbility extends LaravelEntrustMiddleware
             $permissions = explode(self::DELIMITER, $permissions);
         }
 
+        $guard = (is_array($options) && isset($options[0])) ? $options[0] : Config::get('entrust.defaults.guard');
+        $validateAll = (is_array($options) && isset($options[1])) ? $options[1] : false;
+
         if (!is_bool($validateAll)) {
             $validateAll = filter_var($validateAll, FILTER_VALIDATE_BOOLEAN);
         }
 
-        if ($this->auth->guest() || !$request->user()->ability($roles, $permissions, [ 'validate_all' => $validateAll ])) {
+        if (Auth::guard($guard)->guest() || !Auth::guard($guard)->user()->ability($roles, $permissions, [ 'validate_all' => $validateAll ])) {
             return $this->unauthorized();
         }
 
